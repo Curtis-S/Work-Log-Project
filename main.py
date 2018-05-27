ad143dbfa8346aaa8661e9dd7bf88410c45482f6
Curtis S <curtis_scott@live.co.uk>
2018-05-27 14:07:20 +0100

    files added

---
+++ main.py
@@ -0,0 +1,12 @@
+print("hello welcome to work log recorder script")
+while True:
+    first_response = input("please choose one of the options\n"
+                           "1. Add new entry.\n"
+                           "2. Search entry.\n"
+                           "3. Quit\n")
+    if first_response == "1":
+        print()
+    elif first_response == "2":
+        print()
+    elif first_response == "3":
+        break
