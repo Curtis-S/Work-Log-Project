29f7a318bf5cb6c2d17f54c9e1523616626b4c5d
Curtis S <curtis_scott@live.co.uk>
2018-05-30 16:55:47 +0100

    v1 all methods operational

---
+++ load_write_functions.py
@@ -0,0 +1,85 @@
+import csv
+import datetime
+import re
+
+
+class File_Reader:
+
+    def load_csv_file(self):
+        with open('work_log.csv', newline='') as csvfile:
+            work_loads = csv.DictReader(csvfile)
+            rows = list(work_loads)
+        return rows
+
+
+class FileWriter:
+
+    def write_entry_first_time(self, task, time_spent, notes, date):
+        real_date = datetime.strftime("%d/%m/%y")
+
+        with open('work_log.csv', 'a+', newline='') as csvfile:
+            real_date = datetime.datetime.strptime(date, "%d/%m/%Y")
+            with open('work_log.csv', 'a', newline='') as csvfile:
+                fieldnames = ['task', 'time_spent', 'notes', 'date']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writerow(
+                    {'task': task, 'time_spent': time_spent, 'notes': notes,
+                     'date': real_date.strftime("%d/%m/%Y")})
+
+    def write_entry_append(self, task, time_spent, notes, date):
+        real_date = datetime.datetime.strptime(date, "%d/%m/%Y")
+        with open('work_log.csv', 'a', newline='') as csvfile:
+            fieldnames = ['task', 'time_spent', 'notes', 'date']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow(
+                {'task': task, 'time_spent': time_spent, 'notes': notes,
+                 'date': real_date.strftime("%d/%m/%Y")})
+
+
+class DataSearch:
+    def find_by_date(self, date):
+        file_ob = File_Reader()
+        data = file_ob.load_csv_file()
+        return_list = []
+        real_date = datetime.datetime.strptime(date, "%d/%m/%Y")
+        for entries in data:
+            if entries.get("date") == real_date.strftime("%d/%m/%Y"):
+                return_list.append(entries)
+
+        return return_list
+
+    def find_by_time_spent(self, time):
+        realtime = int(time)
+        file_ob = File_Reader()
+        data = file_ob.load_csv_file()
+        return_list = []
+        for entries in data:
+            if int(entries.get("time_spent")) == realtime:
+                return_list.append(entries)
+
+        return return_list
+
+    def find_by_name(self, name):
+        file_ob = File_Reader()
+        data = file_ob.load_csv_file()
+        return_list = []
+        for entries in data:
+            if name in entries.get("task") or name in entries.get("notes"):
+                return_list.append(entries)
+
+        return return_list
+
+    def find_by_regex(self, pattern):
+        file_ob = File_Reader()
+        data = file_ob.load_csv_file()
+        return_list = []
+        search_pattern = re.compile(pattern)
+        for entries in data:
+            if search_pattern.search(
+                    entries.get("task")) or search_pattern.search(
+                    entries.get("notes")) is not None:
+                return_list.append(entries)
+            else:
+                pass
+
+        return return_list
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import load_write_functions
+import os
+
 print("hello welcome to work log recorder script")
 while True:
     first_response = input("please choose one of the options\n"
@@ -5,8 +8,184 @@ while True:
                            "2. Search entry.\n"
                            "3. Quit\n")
     if first_response == "1":
-        print()
+        os.system('clear')
+        second_response = input("is this your first time using this script\n"
+                                "because the script will add the csv column\n"
+                                "headers if it is\n"
+                                "type Y for yes or anything else for no :")
+        if second_response.lower() == "y":
+            while True:
+                add_entry_ob = load_write_functions.FileWriter()
+                try:
+                    add_entry_ob.write_entry_first_time(
+                        input("enter task name: "),
+                        int(input("enter minutes spent"
+                                  "(numbers only "
+                                  "please): ")),
+                        input("enter any notes if any: ")
+                        , input("enter date in "
+                                "dd/mm/yyyy format: "))
+                    os.system('clear')
+                    break
+                except ValueError:
+                    print(
+                        "please enter numbers for minute"
+                        " spent and a date in the"
+                        "dd/mm/yyyy format to continue")
+
+        else:
+            while True:
+                add_entry_ob = load_write_functions.FileWriter()
+                try:
+                    add_entry_ob.write_entry_append(input("enter task name: "),
+                                                    int(input(
+                                                        "enter minutes spent"
+                                                        "(numbers only "
+                                                        "please): ")),
+                                                    input(
+                                                        "enter any"
+                                                        " notes if any: ")
+                                                    , input("enter"
+                                                            " date in "
+                                                            "dd/mm/yyyy "
+                                                            "format: "))
+                    os.system('clear')
+                    break
+                except ValueError:
+                    print(
+                        "please enter numbers for minute"
+                        " spent and a date in the"
+                        "dd/mm/yyyy format to continue")
     elif first_response == "2":
-        print()
+        os.system('clear')
+        search_ob = load_write_functions.DataSearch()
+        while True:
+            second_response = input("please choose a way to search\n"
+                                    "1. by name.\n"
+                                    "2. by date.\n"
+                                    "3. by minutes \n"
+                                    "4. by regex\n"
+                                    "5. previous menu\n")
+            if second_response == "1":
+                os.system('clear')
+                name_list = search_ob.find_by_name(
+                    input("enter name of task: "))
+                for index, entries in enumerate(name_list):
+                    print("             {} out of {}\n"
+                          "task: {}\n"
+                          "time spent: {} minutes\n"
+                          "notes: {}\n"
+                          "date {}\n".format(index + 1,
+                                             len(name_list),
+                                             entries.get("task"),
+                                             entries.get("time_spent"),
+                                             entries.get("notes"),
+                                             entries.get("date")))
+                    third_response = input(
+                        "type e to escape any key to see next entry (if any"
+                        ")")
+                    if third_response.lower() == "e":
+                        break
+                    os.system('clear')
+
+            if second_response == "2":
+                os.system('clear')
+                while True:
+                    try:
+                        date_list = search_ob.find_by_date(
+                            input("enter a date to "
+                                  "search (dd/mm/yyyy"
+                                  "format.): "))
+                        for index, entries in enumerate(date_list):
+                            print("             {} out of {}\n"
+                                  "task: {}\n"
+                                  "time spent: {} minutes\n"
+                                  "notes: {}\n"
+                                  "date {}\n".format(index + 1,
+                                                     len(date_list),
+                                                     entries.get("task"),
+                                                     entries.get("time_spent"),
+                                                     entries.get("notes"),
+                                                     entries.get("date")))
+                            third_response = input(
+                                "type e to escape any key to"
+                                " see next entry (if any"
+                                ")")
+                            if third_response.lower() == "e":
+                                break
+                            os.system('clear')
+                        break
+                    except ValueError:
+                        print(
+                            "please enter a valid date dd/mm/yyyy format")
+
+            if second_response == "3":
+                os.system('clear')
+                while True:
+                    try:
+                        time_list = search_ob.find_by_time_spent(
+                            input("enter how much"
+                                  " minutes to "
+                                  "the"
+                                  " task took: "))
+                        for index, entries in enumerate(time_list):
+                            print("             {} out of {}\n"
+                                  "task: {}\n"
+                                  "time spent: {} minutes\n"
+                                  "notes: {}\n"
+                                  "date {}\n".format(index + 1,
+                                                     len(time_list),
+                                                     entries.get("task"),
+                                                     entries.get("time_spent"),
+                                                     entries.get("notes"),
+                                                     entries.get("date")))
+                            third_response = input(
+                                "type e to escape any key to "
+                                "see next entry (if any"
+                                ")\n")
+                            if third_response.lower() == "e":
+                                break
+                            os.system('clear')
+                        break
+                    except ValueError:
+                        print(
+                            "please enter a whole number eg. '600' ")
+
+            if second_response == "4":
+                os.system('clear')
+                while True:
+                    try:
+                        regex_match_list = search_ob.find_by_regex(
+                            input("enter regex "
+                                  "pattern: "))
+                        for index, entries in enumerate(regex_match_list):
+                            print("             {} out of {}\n"
+                                  "task: {}\n"
+                                  "time spent: {} minutes\n"
+                                  "notes: {}\n"
+                                  "date {}\n".format(index + 1,
+                                                     len(regex_match_list),
+                                                     entries.get("task"),
+                                                     entries.get("time_spent"),
+                                                     entries.get("notes"),
+                                                     entries.get("date")))
+                            third_response = input(
+                                "type e to escape any key to "
+                                "see next entry (if any"
+                                ")")
+                            if third_response.lower() == "e":
+                                break
+                            os.system('clear')
+                        break
+                    except ValueError:
+                        print(
+                            "please enter a valid  regex pattern\n\n")
+
+            if second_response == "5":
+                break
+
     elif first_response == "3":
         break
+    else:
+        os.system('clear')
+        print("please choose a option from the list\n")
